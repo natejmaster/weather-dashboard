ffa208adb8027f3c263c7872c0d87cfa10b259d8
natejmaster@gmail.com <natejmaster@gmail.com>
2023-07-18 16:59:02 -0500

    Declared basic interactive variables in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
 $(function () {
-let weatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=03829b78f9fbd15989252f9ded900d22`
+let weatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=03829b78f9fbd15989252f9ded900d22`;
+let today = dayjs();
+let locationSearch =  $('#location-search');
+let searchHistory = $('#search-history');
 });
