f0f0c7b32ae127cb593695ed95948b73afa7c69e
natejmaster@gmail.com <natejmaster@gmail.com>
2023-07-18 17:45:37 -0500

    Crafted the findCoords function to take the input from locationSearch and log the closest matches to the console--needs more work

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,29 @@ $(function () {
 let weatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=03829b78f9fbd15989252f9ded900d22`;
 let today = dayjs();
 let locationSearch =  $('#location-search');
+let searchButton = $('#location-search-button');
 let searchHistory = $('#search-history');
 
 //Take the value entered into the locationSearch input
     //Use this value with some third party API to match location name or zip code to long and lat
-    //Use long and lat in function to call weatherApi
+    function findCoords() {
+        let reverseGeoCoderUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${locationSearch.val()}&limit=5&appid=03829b78f9fbd15989252f9ded900d22`;
+        fetch(reverseGeoCoderUrl)
+            .then(function (response) {
+                if (response.ok) {
+                  return response.json();
+                } else {
+                  throw new Error('Error: ' + response.status);
+                }
+              })
+              .then(function (data) {
+                console.log(data);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+          }
+    //Use lat and long in function to call weatherApi
     //Fetch response from weatherApi
     //Fetch data from response
 //Use data to populate html containers
@@ -23,4 +41,5 @@ let searchHistory = $('#search-history');
         //Populate Data
         //Append forecast cards to five-day-forecast container
 //Event listener for the search button
+searchButton.on('click', findCoords);
 });
