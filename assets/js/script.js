b2d92a7697358973f8b32b512547516b458efc50
natejmaster@gmail.com <natejmaster@gmail.com>
2023-07-18 17:08:29 -0500

    Added js pseudo-code to guide js coding process

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,24 @@ let weatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon
 let today = dayjs();
 let locationSearch =  $('#location-search');
 let searchHistory = $('#search-history');
+
+//Take the value entered into the locationSearch input
+    //Use this value with some third party API to match location name or zip code to long and lat
+    //Use long and lat in function to call weatherApi
+    //Fetch response from weatherApi
+    //Fetch data from response
+//Use data to populate html containers
+    //Populate current-header
+        //Current location name
+        //Current date
+        //Current weather icon
+    //Populate current-conditions
+        //Current temp
+        //Current wind
+        //Current humidity
+    //Populate five-day-forecast
+        //Create elements for five day forecast cards
+        //Populate Data
+        //Append forecast cards to five-day-forecast container
+//Event listener for the search button
 });
