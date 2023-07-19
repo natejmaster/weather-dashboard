f6dc75ee242e15bafb03c7856f30fdad3934b3a9
natejmaster@gmail.com <natejmaster@gmail.com>
2023-07-18 20:18:10 -0500

    Crafted framework of API function that will access the weather API given the latitude and longitude

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 $(function () {
-let weatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=03829b78f9fbd15989252f9ded900d22`;
 let today = dayjs();
 let locationSearch =  $('#location-search');
 let searchButton = $('#location-search-button');
@@ -8,7 +7,7 @@ let searchHistory = $('#search-history');
 //Take the value entered into the locationSearch input
     //Use this value with some third party API to match location name or zip code to long and lat
     function findCoords() {
-        let reverseGeoCoderUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${locationSearch.val()}&limit=5&appid=03829b78f9fbd15989252f9ded900d22`;
+        let reverseGeoCoderUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${locationSearch.val()}&limit=1&appid=03829b78f9fbd15989252f9ded900d22`;
         fetch(reverseGeoCoderUrl)
             .then(function (response) {
                 if (response.ok) {
@@ -25,6 +24,23 @@ let searchHistory = $('#search-history');
               });
           }
     //Use lat and long in function to call weatherApi
+    function getWeather(lat, long) {
+        let weatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=03829b78f9fbd15989252f9ded900d22`;
+        fetch(weatherApi)
+        .then(function (response) {
+            if (response.ok) {
+              return response.json();
+            } else {
+              throw new Error('Error: ' + response.status);
+            }
+          })
+          then(function (data) {
+            console.log(data);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
     //Fetch response from weatherApi
     //Fetch data from response
 //Use data to populate html containers
